140564caef417f4e8a1e0596897f038e515afc4e
geraltevileye <yovanicrat@gmail.com>
2025-12-08 14:47:00 -0600

    Update app_viejo_antes_de_final.py

--- app_viejo_antes_de_final.py
+++ app_viejo_antes_de_final.py
@@ -295,7 +295,7 @@ def agregar_pasajero():
     apellidos = request.form['apellidos']
     pasaporte = request.form['pasaporte']
     nacionalidad = request.form['nacionalidad']
-    fecha_nacimiento = request.form['fecha_nacimiento'] if request.form['fecha_nacimiento'] else None
+    vuelo= request.form['vuelo'] if request.form['vuelo'] else None
     correo = request.form['correo']
     telefono = request.form['telefono']
     
@@ -303,9 +303,9 @@ def agregar_pasajero():
     cursor = conn.cursor()
     try:
         cursor.execute('''
-            INSERT INTO Pasajeros (nombre, apellidos, pasaporte, nacionalidad, fecha_nacimiento, correo, telefono)
+            INSERT INTO Pasajeros (nombre, apellidos, pasaporte, nacionalidad, vuelo, correo, telefono)
             VALUES (%s, %s, %s, %s, %s, %s, %s)
-        ''', (nombre, apellidos, pasaporte, nacionalidad, fecha_nacimiento, correo, telefono))
+        ''', (nombre, apellidos, pasaporte, nacionalidad, vuelo, correo, telefono))
         conn.commit()
         
         log_operacion('ALTA', 'Pasajeros', pasaporte, f'Pasajero agregado: {nombre} {apellidos}')
@@ -622,7 +622,7 @@ def editar_pasajero(id):
     apellidos = request.form['apellidos']
     pasaporte = request.form['pasaporte']
     nacionalidad = request.form['nacionalidad']
-    fecha_nacimiento = request.form['fecha_nacimiento'] if request.form['fecha_nacimiento'] else None
+    vuelo = request.form['vuelo'] if request.form['vuelo'] else None
     correo = request.form['correo']
     telefono = request.form['telefono']
     
@@ -632,9 +632,9 @@ def editar_pasajero(id):
         cursor.execute('''
             UPDATE Pasajeros 
             SET nombre = %s, apellidos = %s, pasaporte = %s, nacionalidad = %s, 
-                fecha_nacimiento = %s, correo = %s, telefono = %s
+                vuelo = %s, correo = %s, telefono = %s
             WHERE id_pasajero = %s
-        ''', (nombre, apellidos, pasaporte, nacionalidad, fecha_nacimiento, correo, telefono, id))
+        ''', (nombre, apellidos, pasaporte, nacionalidad, vuelo, correo, telefono, id))
         conn.commit()
         
         log_operacion('EDICION', 'Pasajeros', id, f'Pasajero actualizado: {nombre} {apellidos}')
